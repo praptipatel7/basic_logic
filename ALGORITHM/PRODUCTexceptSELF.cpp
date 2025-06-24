5839f736e1a23cc856c2e16c3cb0b914d6c9b5a8
praptipatel7 <patelprapti0737@gmail.com>
2025-06-24 21:40:05 +0530

    24-06-2025

--- ALGORITHM/PRODUCTexceptSELF.cpp
+++ ALGORITHM/PRODUCTexceptSELF.cpp
@@ -31,3 +31,26 @@
 
 //============================================================================
 //Optimal solution
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> nums = {1,2,3,4};
+    int n=nums.size();
+    vector<int> ans(n, 1);
+    //prefix => ans
+    for(int i=1 ; i<n ; i++){
+        ans[i] = ans[i-1]*nums[i-1];
+    }
+    int suffix =1;
+    //suffix
+    for(int i=n-2 ; i>=0 ; i--){
+        suffix *=nums[i+1];   
+        ans[i]*=suffix;
+    }
+    for(int val:ans){
+        cout<<val<<"   ";
+    }
+    return 0;
+}
