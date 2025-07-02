54e50968c965834a44524a4402a5e5a9f3c9274c
praptipatel7 <patelprapti0737@gmail.com>
2025-07-02 19:17:16 +0530

    09-06-2025 Updated

---
+++ ALGORITHM/PeakMountain.cpp
@@ -0,0 +1,9 @@
+//============================================================
+//Peak Index in mountain Array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    
+}
--- ALGORITHM/RotatedSorted.cpp
+++ ALGORITHM/RotatedSorted.cpp
@@ -1,2 +1,33 @@
 //==================================================================
 //Search in Rotated Sorted array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> nums={4,5,6,7,0,1,2};
+    int target = 0;
+    int start=0, end=nums.size()-1;
+    
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            cout<<mid;
+        }
+        if(nums[start]<=nums[mid]){//left sorted
+            if(nums[start]<= target && target<=nums[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        else{//right sorted
+            if(nums[mid]<= target && target<=nums[end]){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+    }
+    return 0;    
+}
