6fe9e2cee6047cdd094b209ea9e48d3ce3339af5
praptipatel7 <patelprapti0737@gmail.com>
2025-07-17 23:35:55 +0530

    Day-2 Strings 17-07-2025

--- String/Compression.cpp
+++ String/Compression.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    
+    vector<char> chars= {'a','a','b','b','c','c','c'};
+    int n = chars.size();
+    int idx=0;
+    for(int i=0 ; i<n;i++){
+        int count=0;
+        char ch=chars[i];
+        while(i<n && chars[i]==ch){
+            i++;count++;
+        }
+        if(count==1) chars[idx++]=ch;
+        else {
+            chars[idx++]=ch;
+            string str = to_string(count);
+            for(char dig:str){
+                chars[idx++]=dig;
+            }
+        }
+        i--;
+    }
+    chars.resize(idx);
+    for(char abc:chars){
+        cout<<abc<<"  ";
+    }
+    return 0;        
 }
